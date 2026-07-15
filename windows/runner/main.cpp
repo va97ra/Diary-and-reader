3ec8b767c33b81f4756da9fc95422dfaaefc5a74
va97ra <122785470+va97ra@users.noreply.github.com>
2026-07-15 18:03:48 +0300

    Fix Windows Unicode application metadata

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -27,7 +27,8 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   FlutterWindow window(project);
   Win32Window::Point origin(10, 10);
   Win32Window::Size size(1280, 720);
-  if (!window.Create(L"Дневник", origin, size)) {
+  if (!window.Create(L"\u0414\u043d\u0435\u0432\u043d\u0438\u043a", origin,
+                     size)) {
     return EXIT_FAILURE;
   }
   window.SetQuitOnClose(true);
